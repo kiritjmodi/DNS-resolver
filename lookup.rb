91cc2f9d1a1e741dfec326fbbf54abd5d6ef52b5
kiritjmodi <76820014+kiritjmodi@users.noreply.github.com>
2021-11-11 13:00:06 +0530

    update in dns resolver

--- lookup.rb
+++ lookup.rb
@@ -17,17 +17,41 @@ domain = get_command_line_argument
 # array of string, where each element is a line
 # https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
+# puts dns_raw
 
+def parse_dns(dns_raw)
+  dns_rec={}
+  dns_raw.
+  reject {|ln| ln.empty? }.
+  map {|ln| ln.strip.split(", ") }.
+  reject do |rec1|
+    #puts rec1
+    rec1.length==0  || rec1[0].start_with?('#')
+  end.
+  each_with_object ({}) do | rec1, rec2 |
+    #puts rec1.to_s + "---" + rec2.to_s
+    dns_rec[rec1[1]] = {type:rec1[0], target:rec1[2]}
+  end
+  #puts dns_rec
+  dns_rec
+end
+def resolve (dns_rec, lookup_chain,domain)
+  rec1=dns_rec[domain]
+  if (!rec1)
+    return["Error: rec not found #{domain}"]
+  elsif rec1 [:type]== "CNAME"
+    lookup_chain.push (rec1[:target])
+    resolve(dns_rec,lookup_chain,rec1[:target])
+  elsif rec1[:type]=="A"
+  lookup_chain.push(rec1[:target])
+  return lookup_chain
+  else
+    rerurn ["Invalid rec type #{domain}"]
+  end
+end
+  # ..
 # ..
-# ..
-# FILL YOUR CODE HERE
-p Resolv.getaddress "www.google.com"
-p Resolv.getname "172.217.163.46"
-
-Resolv::DNS.open do |dns|
-  ress = dns.getresources "www.google.com", Resolv::DNS::Resource::IN::A
-  p ress.map(&:address)
- end
+#
 # To complete the assignment, implement `parse_dns` and `resolve`.
 # Remember to implement them above this line since in Ruby
 # you can invoke a function only after it is defined.
